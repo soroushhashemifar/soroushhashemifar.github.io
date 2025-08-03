e0e80588e976a6fb7a74d9ff4c4abf4605c127e2
soroushhashemifar <soroush.hashemi76@gmail.com>
2025-08-03 12:48:16 +0000

    Deploying to gh-pages from @ soroushhashemifar/soroushhashemifar.github.io@de5f1e37760552f171e2ee9efdcd6cdc3b534272 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
